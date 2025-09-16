e1a051539c5bb20edca2369a43f74207aeab494b
Shahaf Bahar <sbahar@redhat.com>
2025-09-16 16:59:00 +0300

    refactor

---
+++ test/e2e/ownership_test.go
@@ -0,0 +1,54 @@
+package e2e
+
+import (
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/sbahar619/githubIssue-operator-assignment/internal/github"
+	"github.com/sbahar619/githubIssue-operator-assignment/internal/utils"
+)
+
+var _ = Describe("GitHub Issue Ownership", func() {
+	var namespace string
+	var timestamp int64
+
+	BeforeEach(func() {
+		timestamp = time.Now().Unix()
+		namespace = fmt.Sprintf("test-ns-%d", timestamp)
+
+		By("Creating test namespace")
+		createNamespace(namespace)
+	})
+
+	AfterEach(func() {
+		By("Cleaning up test namespace")
+		deleteNamespace(namespace)
+	})
+
+	It("Should detect externally owned issue", func() {
+		title := fmt.Sprintf("External-Issue-Test-%d", timestamp)
+
+		By("Creating GitHub issue directly without operator labels")
+		token, err := getOperatorToken()
+		Expect(err).NotTo(HaveOccurred())
+		githubClient, err := github.NewClient(token, githubRepoURL)
+		Expect(err).NotTo(HaveOccurred())
+
+		issue, err := githubClient.CreateIssue(ctx, title, "External issue")
+		Expect(err).NotTo(HaveOccurred())
+		DeferCleanup(githubClient.CloseIssue, ctx, issue.GetNumber())
+
+		By("Creating CR with same title")
+		crName := fmt.Sprintf("external-test-%d", timestamp)
+		cr := newGithubIssue(crName, namespace, title, nil)
+		Expect(k8sClient.Create(ctx, cr)).To(Succeed())
+
+		By("Expecting external ownership detection")
+		Eventually(func() bool {
+			return hasConditionWithReason(cr, utils.ReasonExternallyOwnedIssue)
+		}, timeout, pollInterval).Should(BeTrue())
+	})
+})
